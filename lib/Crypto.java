cb7f2fe789d1c8e226008c745d5a73b9113c83a7
Cory Sabol <sible777@gmail.com>
2017-03-08 16:38:19 -0500

    create project structure

---
+++ lib/Crypto.java
@@ -0,0 +1,9 @@
+/*
+ * Crypto class shell
+ * Eventually will house the crypto algorithms and helpers to perfom all the
+ * crypto needs for the Client and Server applicaitons.
+ */
+
+class Crypto {
+
+}
---
+++ test/Test.java
@@ -0,0 +1,9 @@
+/*
+ * Just a class shell so that it may be pushed to github
+ * Possibly use JUnit to create the tests, though I'm not sure how JUnit behaves
+ * without an IDE.
+ */
+
+class Test {
+
+}

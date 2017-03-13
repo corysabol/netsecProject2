e1982950025d68a5cc2b89c5efb143e70b521f23
Cory Sabol <sible777@gmail.com>
2017-03-13 11:37:37 -0400

    modify test to compare base64 encoded results

--- test/CryptoTest.java
+++ test/CryptoTest.java
@@ -16,15 +16,15 @@ public class CryptoTest {
     // set up the DES cypher
     DES.setCrypto(Crypto.CIPHER_ALGORITHM.DES);
     String clearText = "Network Security";
-    String correctMsg_b64 = "2CP9Rtr+ZR4pPh8Gup6URg==";
+    String correctMsg_b64 = "U0yZMyTQ+9bGRqifSxpkPg==";
     byte[] clearBytes = clearText.getBytes();
     byte[] cipherBytes;
     String cipherText;
 
     // === TEST KEY ===
-    String testKey_b64 = "Fe+/ve+/vVI777+977+977+9Cg==";
+    String testKey_b64 = "4PJ5ige6GrM=";
     byte[] testKey_raw = Base64.getDecoder().decode(testKey_b64.getBytes());
-    String testKey_rawStr = new String(testKey_raw, StandardCharsets.UTF_8);
+    String testKey_rawStr = new String(testKey_raw);
     // Build a SecretKey instance
     SecretKey testKey_inst = new SecretKeySpec(testKey_raw, 0, testKey_raw.length, "DES");
 
@@ -32,9 +32,9 @@ public class CryptoTest {
     DES.getCipher().init(DES.getCipher().ENCRYPT_MODE, testKey_inst);
     // === PERFORM DES ENCRYPTION ON MESSAGE ===
     cipherBytes = DES.getCipher().doFinal(clearBytes);
-    cipherText = new String(cipherBytes, StandardCharsets.UTF_8); // Encode the message with UTF-8
+    byte[] cipherBytes_b64 = Base64.getEncoder().encode(cipherBytes);
+    cipherText = new String(cipherBytes); // Encode the message with UTF-8
 
-    assertEquals(correctMsg_b64, new String(Base64.getEncoder().encode(cipherText.getBytes()),
-                                            StandardCharsets.UTF_8));
+    assertEquals(correctMsg_b64, new String(cipherBytes_b64));
   }
 }

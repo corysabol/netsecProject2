c61f5047376c3492828e2f556ab4414022234aea
Cory Sabol <sible777@gmail.com>
2017-03-16 21:45:37 -0400

    testing decrypt DES

--- test/CryptoTest.java
+++ test/CryptoTest.java
@@ -45,4 +45,40 @@ public class CryptoTest {
 
     assertEquals(correctMsg_b64, new String(cipherBytes_b64));
   }
+
+  @Test
+  public void decryptsMessage_DES() {
+    // instance of crypto helper
+    Crypto DES = new Crypto();
+    // set up the DES cypher
+    DES.setCrypto(Crypto.CIPHER_ALGORITHM.DES);
+    
+    // === TEST DATA ===
+    String correctMsg = "Network Security";
+    String encryptedMsg_b64 = "ZOF84xPpY65rfuZ3+Edicw==";
+
+    // === TEST IV ===
+    IvParameterSpec testIV = new IvParameterSpec("dGVzdGl2".getBytes());
+    byte[] clearBytes = clearText.getBytes();
+    byte[] cipherBytes;
+    String cipherText;
+
+    // === TEST KEY ===
+    String testKey_b64 = "4PJ5ige6GrM=";
+    byte[] testKey_raw = Base64.getDecoder().decode(testKey_b64.getBytes());
+    String testKey_rawStr = new String(testKey_raw);
+
+    // === Build a SecretKey instance ===
+    SecretKey testKey_inst = new SecretKeySpec(testKey_raw, 0, testKey_raw.length, "DES");
+    
+    // === Set the key ===
+    DES.setSecretKey(testKey_inst);
+
+    // === PERFORM DES ENCRYPTION ON MESSAGE ===
+    cipherBytes = DES.encrypt(clearBytes, testIV);
+    byte[] cipherBytes_b64 = Base64.getEncoder().encode(cipherBytes);
+    cipherText = new String(cipherBytes);
+
+    assertEquals(correctMsg, new String(decryptedMsg));   
+  }
 }
